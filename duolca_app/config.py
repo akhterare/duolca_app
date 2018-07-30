1154a2cda815363a6a79a1f13c9f37d15a05567a
akhterare <40837881+akhterare@users.noreply.github.com>
2018-07-30 11:45:57 -0400

    Token-Works-On-Local-Only

--- duolca_app/config.py
+++ duolca_app/config.py
@@ -1,5 +1,5 @@
-RESOURCE = "https://graph.microsoft.com"  # Add the resource you want the access token for
-TENANT = "nokia.onmicrosoft.com"  # Enter tenant name, e.g. contoso.onmicrosoft.com
-AUTHORITY_HOST_URL = "https://login.microsoftonline.com"
-CLIENT_ID = "fb20d6fe-ce09-449a-b096-90f229943863"  # copy the Application ID of your app from your Azure portal
-CLIENT_SECRET = "JSiaZJi/vYOYdebCTQNQ15RMmjJlVbCQfMYI5WQFTi4="  # copy the value of key you generated when setting up the application
+# RESOURCE = "https://graph.microsoft.com"  # Add the resource you want the access token for
+# TENANT = "nokia.onmicrosoft.com"  # Enter tenant name, e.g. contoso.onmicrosoft.com
+# AUTHORITY_HOST_URL = "https://login.microsoftonline.com"
+# CLIENT_ID = "fb20d6fe-ce09-449a-b096-90f229943863"  # copy the Application ID of your app from your Azure portal
+# CLIENT_SECRET = "JSiaZJi/vYOYdebCTQNQ15RMmjJlVbCQfMYI5WQFTi4="  # copy the value of key you generated when setting up the application
--- duolca_app/views.py
+++ duolca_app/views.py
@@ -5,18 +5,20 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template, Flask, Response, request, session
 import flask 
-from adal import AuthenticationContext
+import requests
+import adal
 from duolca_app import app
 from duolca_app import config
+import os 
+import json 
 
 # SETTING VALUES USED FOR ACCESS TOKEN ACQUISITION
-PORT = 5000  # A flask app by default runs on PORT 5000
-AUTHORITY_URL = config.AUTHORITY_HOST_URL + '/' + config.TENANT
-REDIRECT_URI = 'http://localhost:{}/getAToken'.format(PORT)
-TEMPLATE_AUTHZ_URL = ('https://login.microsoftonline.com/{}/oauth2/authorize?' +
-                      'response_type=code&client_id={}&redirect_uri={}&' +
-                      'state={}&resource={}')
-
+# PORT = 5000  # A flask app by default runs on PORT 5000
+# AUTHORITY_URL = config.AUTHORITY_HOST_URL + '/' + config.TENANT
+# REDIRECT_URI = 'http://localhost:{}/getAToken'.format(PORT)
+# TEMPLATE_AUTHZ_URL = ('https://login.microsoftonline.com/{}/oauth2/authorize?' +
+#                       'response_type=code&client_id={}&redirect_uri={}&' +
+#                       'state={}&resource={}')
 
 @app.route('/')
 @app.route('/home')
@@ -28,8 +30,23 @@ def home():
         year=datetime.now().year,
     )   
 
-@app.route("/getAToken")
+@app.route("/getAToken", methods=['GET', 'POST'])
 def auth():
+    URL = "https://duolcaapp.azurewebsites.net/.auth/me"
+    r = requests.get(URL)
+    right_header = r.headers
+
+    tenant = "5d471751-9675-428d-917b-70f44f9630b0"
+    authority_url = 'https://login.microsoftonline.com/' + tenant
+    client_id = 'fb20d6fe-ce09-449a-b096-90f229943863'
+    client_secret = '9bPMwvy7HztrwVkkCR08BOPMbPUb5Ze8MqMVZOwGTMQ='
+    resource = 'https://management.azure.com'
+
+    context = adal.AuthenticationContext(authority_url)
+    token = context.acquire_token_with_client_credentials(resource, client_id, client_secret)
+    access_token = token['accessToken']
+
+    # access_token = flask.request.headers["X-MS-TOKEN-AAD-ACCESS-TOKEN"]
     # code = flask.request.args['code']
     # state = flask.request.args['state']
     # if state != Flask.flask.session['state']:
@@ -42,8 +59,9 @@ def auth():
 
     return render_template(
         'auth.html', 
-        title='Authorization'
-        message='Your Token'
+        title='Authorization',
+        message='Your Token Was Successfully Retrieved!',
+        access_token=access_token
     )
 
 @app.route('/contact')
@@ -64,6 +82,6 @@ def about():
         'about.html',
         title='About',
         year=datetime.now().year,
-        message='Your application description page.'
+        message='Duolca App Documentation.'
     )
 
